4edafc1b19063ab9fe1c56eac0fc7c65f9de85e1
adelandrew049-cpu <adelandrew049@gmail.com>
2026-07-28 13:29:06 +0300

    Update json.js

---
+++ task 7/json.js
@@ -0,0 +1,35 @@
+[
+  {
+    "id": 1,
+    "name": "Ahmed",
+    "subject": "Math",
+    "grade": 95
+  },
+  {
+    "id": 2,
+    "name": "Sara",
+    "subject": "Physics",
+    "grade": 88
+  }
+][
+  {
+    "id": 1,
+    "name": "Ahmed",
+    "subject": "Math",
+    "grade": 95
+  },
+  {
+    "id": 2,
+    "name": "Sara",
+    "subject": "Physics",
+    "grade": 98
+  }
+]
+[
+  {
+    "id": 2,
+    "name": "Sara",
+    "subject": "Physics",
+    "grade": 98
+  }
+]
